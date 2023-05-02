ee9c2ff7cabce02d1cf64aab8db0920f6ab89292
Manu Partanen <manu111.partanen@gmail.com>
2023-05-02 20:18:00 +0300

    Added dropdown component.

---
+++ 03_NotesApp_(React)/NotesApp/src/components/dropdown/dropdown.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./dropdown.css";
+
+function Dropdown(props) {
+    const [selectedOption, setSelectedOption] = useState(props.defaultOption);
+  
+    const handleOptionSelect = (event) => {
+      setSelectedOption(event.target.value);
+      props.onOptionSelect(event.target.value);
+    };
+  
+    const optionElements = props.options.map((option) => (
+      <option key = {option.value} value = {option.value}>
+        {option.label}
+      </option>
+    ));
+  
+    return(
+      <label className = "SelectCourse">
+        Select course:
+        <select className = "CoursesDropdown" value = {selectedOption} onChange = {handleOptionSelect}>
+            {optionElements}
+        </select>
+      </label>
+    );
+  }
+
+export default Dropdown;
--- 03_NotesApp_(React)/NotesApp/src/pages/add_note/add_note.jsx
+++ 03_NotesApp_(React)/NotesApp/src/pages/add_note/add_note.jsx
@@ -1,47 +1,77 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "/src/components/header/header.jsx";
+import Dropdown from "/src/components/dropdown/dropdown.jsx";
 import "./add_note.css";
 
 function AddNotePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [selectedCourse, setSelectedCourse] = useState("");
 
-    function AddNote() {
-        if (document.getElementById("ta1").value == "") {
-            document.getElementById("ta1").value = "Text area cannot be empty!";
-        }else if (document.getElementById("ta1").value != "") {
-            document.getElementById("ta1").value = "Note added!";
-        }
+  function AddNote() {
+    const textField = document.getElementById("ta1");
+
+    if (textField.value = "Text area cannot be empty!") {
+        textField.value = "";
+    } else if (textField.value == "Note added to course!") {
+      textField.value = "";
+    } else if (textField.value == "") {
+        textField.value = "Text area cannot be empty!";
+    } else {
+        textField.value = "Note added to course!";
     }
+  }
 
-    function HandleKeyDown(event) {
-        if (event.key == "Enter") {
-          AddNote();
-        }
-      }
+  function HandleKeyDown(event) {
+    if (event.key == "Enter") {
+      AddNote();
+    }
+  }
 
-    return(
-        <div>
-            <Header/>
-            <div className = "AddNotePageContainer">
-                <div className = "AddNoteContent">
-                    <div className = "AddNote">
-                        <h2>Add note</h2>
-                    </div>
-                    <div className = "CourseSelector">
-                        
-                    </div>
-                    <div className = "TextField">
-                        <h3>Write note:</h3>
-                        <textarea id = "ta1" name = "Text1" cols = "40" rows = "5" onKeyDown = {HandleKeyDown}></textarea>
-                        <button onClick = {AddNote}>Add note</button>
-                    </div>
-                    <div className = "BackButton">
-                        <button onClick = {() => navigate("/")}>Back</button>
-                    </div>
-                </div>
-            </div>
+  const HandleCourseSelect = (selectedOption) => {
+    setSelectedCourse(selectedOption);
+  };
+
+  const courseOptions = [
+    { value: "Math", label: "Math" },
+    { value: "Science", label: "Science" },
+    { value: "History", label: "History" },
+    { value: "English", label: "English" },
+  ];
+
+  return(
+    <div>
+      <Header/>
+      <div className = "AddNotePageContainer">
+        <div className = "AddNoteContent">
+          <div className = "AddNote">
+            <h2>Add note</h2>
+          </div>
+          <div className = "CourseSelector">
+            <Dropdown
+              options = {courseOptions}
+              defaultOption = {courseOptions[0].value}
+              onOptionSelect = {HandleCourseSelect}
+            />
+          </div>
+          <div className = "TextField">
+            <h3>Write note:</h3>
+            <textarea
+              id = "ta1"
+              name = "Text1"
+              cols = "40"
+              rows = "5"
+              onKeyDown = {HandleKeyDown}
+            ></textarea>
+            <button onClick = {AddNote}>Add note</button>
+          </div>
+          <div className = "BackButton">
+            <button onClick = {() => navigate("/")}>Back</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AddNotePage;
